78462808c60d28f645ffe0f1176e09d74c83de73
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2017-01-07 17:37:53 +0200

    + first commit

---
+++ loader.php
@@ -0,0 +1,3 @@
+<?php
+require_once("src/ErrorReporting.php");
+require_once("src/PHPException.php");
---
+++ src/PHPException.php
@@ -0,0 +1,57 @@
+<?php
+error_reporting(E_ALL);
+
+/**
+ * Exception caught automatically when a PHP error is encountered.
+ */
+class PHPException extends Exception {
+	
+	/**
+	 * Class name to which error handling will be delegated.
+	 * 
+	 * @var string $strErrorHandlerClass
+	 */
+	protected static $strErrorHandlerClassName;
+	
+
+	/**
+	 * Sets name of class to which error handling will be delegated.
+	 * 
+	 * @param string $strClassName
+	 */
+	public static function setErrorHandler($strClassName) {
+		self::$strErrorHandlerClassName = $strClassName;
+	}
+	
+	/**
+	 * Function called automatically when a non-fatal PHP error is encountered.
+	 * 
+	 * @param integer $intErrorNumber
+	 * @param string $strMessage
+	 * @param string $strFile
+	 * @param integer $intLine
+	 */
+	public static function nonFatalError($intErrorNumber, $strMessage, $strFile, $intLine) {
+		$e = new self($strMessage, $intErrorNumber);
+		$e->line = $intLine;
+	    $e->file = $strFile;
+	    new self::$strErrorHandlerClassName($e);
+	   	die(); // prevents double-reporting if exception is caught
+	}
+	
+	/**
+	 * Function called automatically when a fatal PHP error is encountered.
+	 */
+	public static function fatalError() {
+		$tblError = error_get_last();
+		if(sizeof($tblError)) {
+			$e = new self($tblError['message'],0);
+			$e->line = $tblError['line'];
+			$e->file = $tblError['file'];
+	        new self::$strErrorHandlerClassName($e);
+		}
+	}
+}
+
+set_error_handler(array("PHPException", "nonFatalError"), E_ALL);
+register_shutdown_function('PHPException::fatalError');
