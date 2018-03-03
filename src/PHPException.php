c76107dd858337a26ac63c7a39bea8e699ebf72e
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2018-03-03 08:34:32 +0200

    - removed useless type identifiers

--- src/PHPException.php
+++ src/PHPException.php
@@ -9,18 +9,18 @@ class PHPException extends Exception {
 	/**
 	 * Object to which error handling will be delegated to.
 	 * 
-	 * @var ErrorHandler $objErrorHandler
+	 * @var ErrorHandler $errorHandler
 	 */
-	protected static $objErrorHandler;
+	private static $errorHandler;
 	
 
 	/**
 	 * Sets object to which error handling will be delegated to.
 	 * 
-	 * @param ErrorHandler $objErrorHandler
+	 * @param ErrorHandler $errorHandler
 	 */
-	public static function setErrorHandler(ErrorHandler $objErrorHandler) {
-		self::$objErrorHandler = $objErrorHandler;
+	public static function setErrorHandler(ErrorHandler $errorHandler) {
+		self::$errorHandler = $errorHandler;
 	}
 
 	/**
@@ -29,23 +29,23 @@ class PHPException extends Exception {
 	 * @return ErrorHandler
 	 */
 	public static function getErrorHandler() {
-		return self::$objErrorHandler;
+		return self::$errorHandler;
 	}
 	
 	/**
 	 * Function called automatically when a non-fatal PHP error is encountered.
 	 * 
-	 * @param integer $intErrorNumber
-	 * @param string $strMessage
-	 * @param string $strFile
-	 * @param integer $intLine
+	 * @param integer $errorNumber
+	 * @param string $message
+	 * @param string $file
+	 * @param integer $line
 	 */
-	public static function nonFatalError($intErrorNumber, $strMessage, $strFile, $intLine) {
-		$e = new self($strMessage, $intErrorNumber);
-		$e->line = $intLine;
-	    $e->file = $strFile;
-	    if(!self::$objErrorHandler) die($strMessage);
-	    self::$objErrorHandler->handle($e);
+	public static function nonFatalError($errorNumber, $message, $file, $line) {
+		$e = new self($message, $errorNumber);
+		$e->line = $line;
+	    $e->file = $file;
+	    if(!self::$errorHandler) die($message);
+	    self::$errorHandler->handle($e);
 	   	die(); // prevents double-reporting if exception is caught
 	}
 	
@@ -53,13 +53,13 @@ class PHPException extends Exception {
 	 * Function called automatically when a fatal PHP error is encountered.
 	 */
 	public static function fatalError() {
-		$tblError = error_get_last();
-		if(sizeof($tblError)) {
-			$e = new self($tblError['message'],0);
-			$e->line = $tblError['line'];
-			$e->file = $tblError['file'];
-			if(!self::$objErrorHandler) die($tblError['message']);
-			self::$objErrorHandler->handle($e);
+		$error = error_get_last();
+		if(sizeof($error)) {
+			$e = new self($error['message'],0);
+			$e->line = $error['line'];
+			$e->file = $error['file'];
+			if(!self::$errorHandler) die($error['message']);
+			self::$errorHandler->handle($e);
 		}
 	}
 }
